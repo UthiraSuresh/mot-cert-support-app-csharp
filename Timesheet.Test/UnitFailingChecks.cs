1c667676e9828d6bc81c48142e3c5b2019f21c0c
UthiraSuresh <uthirasuresh1995@gmail.com>
2026-01-30 12:10:57 +0000

    Failed autromated checks corrected

--- Timesheet.Test/UnitFailingChecks.cs
+++ Timesheet.Test/UnitFailingChecks.cs
@@ -19,14 +19,14 @@ public class UnitFailingChecks
     public void testIncorrectLogin()
     {
         // Mocks the behavior of authDB.checkLogin() method for a fake login attempt.
-        _authDBMock.Setup(_authDBMock => _authDBMock.CheckLogin("fake@test.com", "password")).Returns(new LoginResult(false, null, 0));
+        _authDBMock.Setup(_authDBMock => _authDBMock.CheckLogin("admin@test.com", "password")).Returns(new LoginResult(false, null, 0));
 
         // Calls the login method of authService with fake credentials.
         _authService = new AuthService(_authDBMock.Object);
-        (int code, Credentials credentials) = _authService.Login("admin@test.com", "password123");
+        (int code, Credentials credentials) = _authService.Login("admin@test.com", "password");
 
         // Verifies that the authDB.checkLogin() method is called exactly once with specified arguments.
-        _authDBMock.Verify(_authDBMock => _authDBMock.CheckLogin("fake@test.com", "password"), Times.Once);
+        _authDBMock.Verify(_authDBMock => _authDBMock.CheckLogin("admin@test.com", "password"), Times.Once);
     }
 
     // This is another test method named testValidTokenReturnsOk to validate a session token.
@@ -34,7 +34,7 @@ public class UnitFailingChecks
     public void testValidTokenReturnsOk()
     {
         // Mocks the behavior of authDB.checkSession() method for a valid session token.
-        _authDBMock.Setup(_authDBMock => _authDBMock.CheckSession("abc123", DateTime.Parse("3001-01-01"))).Returns(true);
+        _authDBMock.Setup(_authDBMock => _authDBMock.CheckSession("abc", DateTime.Parse("3001-01-01"))).Returns(true);
 
         // Calls the validate method of authService with a token and date.
         _authService = new AuthService(_authDBMock.Object);
